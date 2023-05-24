1586f70909a1cb3421cb063a4ae3240f0ae55c3f
Furqan Khan <furqankhan5403@gmail.com>
2023-05-24 11:14:55 +0530

    changed model according to the frontend

--- model/model.js
+++ model/model.js
@@ -1,8 +1,24 @@
 const mongoose = require('mongoose')
 
-const loginSchema = new mongoose.Schema({
-    username: {type: String, required: true, min: 3, max:20},
-    password: {type: String, required:true, min: 5}
-})
+const registerSchema = new mongoose.Schema({
+    firstname:{type: String, required: true, min: 3, max:20},
+    surname: {type: String, required: true, max:20},
+    mobile_email:{
+        type: String,
+    required: true,
+    validate: {
+      validator: function(value) {
+        // Regular expression to check if value is a valid email or phone number
+        const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+        const phoneRegex = /^\d+$/;
+        
+        return emailRegex.test(value) || phoneRegex.test(value);
+        }
+      }
+    },
+    password:{type: String, required:true, min:5},
+    dob:{type: Date, required:true},
+    gender:{type: String, required: true},
+});
 
-module.exports = new mongoose.model('Login', loginSchema);
+module.exports = new mongoose.model('register', registerSchema);
