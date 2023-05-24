209218293e766c39c046d427e5a8fa2e116c7c8c
Furqan Khan <furqankhan5403@gmail.com>
2023-05-24 11:13:35 +0530

    connected signup.ejs with backend and saving data in mongoDB

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const path = require('path');
-const Login = require('./model/model')
+const Register = require('./model/model')
 const connectDB = require('./db/connection')
 var bodyParser = require('body-parser');
-var bcrypt = require('bcrypt')
+var bcrypt = require('bcrypt');
+const multer = require('multer');
 require('dotenv').config();
 
 app = express()
+const upload = multer();
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
@@ -27,24 +32,42 @@ app.get('/register', (req, res) => {
 
 
 
-app.post('/login', async (req, res) => {
+app.post('/register', upload.none(), async (req, res) => {
+    const dd =req.body.dd;
+    const mm =req.body.mm;
+    const yyyy =req.body.yyyy;
+
+    const dateofbirth = new Date(yyyy, mm-1, dd);
+    // console.log(req.body.firstname)
+    // console.log(req.body.surname)
+    // console.log(req.body.mobile_email)
+    // console.log(req.body.password)
+    // console.log(dateofbirth);
+    //  console.log(req.body.gender)
+ 
+
     try {
       // Hash the password
-      const hashedPassword = await bcrypt.hash(password, 10);
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
   
       // Create a new user with the hashed password
-      const user = new Login({
-        username,
+      const user = new Register({
+        firstname: req.body.firstname,
+        surname: req.body.surname,
+        mobile_email: req.body.mobile_email,
+        dob : dateofbirth,
+        gender: req.body.gender,
         password: hashedPassword,
       });
       // Save the user to the database
         await user.save();
 
-        res.status(201).json({ message: 'successful login' });
+        //res.status(201).json({ message: 'successful Registration' });
+        res.redirect('/login');
     } catch (error) {
-        res.status(500).json({ error: 'Failed to login user' });
+        res.status(500).json({ error: 'Failed to Register user' });
     }
-});
+ });
 
 
 const port = 3000;
