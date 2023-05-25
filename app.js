882f3b0dcee73673341bd41f82556fd0d91011da
Furqan Khan <furqankhan5403@gmail.com>
2023-05-25 21:28:00 +0530

    completed authentication and displaying users data on the page

--- app.js
+++ app.js
@@ -26,11 +26,9 @@ app.get('/login', (req, res) => {
     res.render('index');
 });
 
-app.get('/register', (req, res) => {
+app.get('/register', async (req, res) => {
     res.render('signup');
 });
-
-
 
 app.post('/register', upload.none(), async (req, res) => {
     const dd =req.body.dd;
@@ -44,14 +42,14 @@ app.post('/register', upload.none(), async (req, res) => {
     // console.log(req.body.password)
     // console.log(dateofbirth);
     //  console.log(req.body.gender)
- 
+   
 
     try {
       // Hash the password
       const hashedPassword = await bcrypt.hash(req.body.password, 10);
   
       // Create a new user with the hashed password
-      const user = new Register({
+        const user = new Register({
         firstname: req.body.firstname,
         surname: req.body.surname,
         mobile_email: req.body.mobile_email,
@@ -65,9 +63,61 @@ app.post('/register', upload.none(), async (req, res) => {
         //res.status(201).json({ message: 'successful Registration' });
         res.redirect('/login');
     } catch (error) {
+        res.redirect('/register');
         res.status(500).json({ error: 'Failed to Register user' });
     }
  });
+
+
+ app.post('/login', upload.none(), async (req,res)=>{
+
+    try{
+    const email_phone = req.body.email_phone;
+    //console.log(email_phone);
+    const pass = req.body.pass;
+    // console.log(pass)
+    const user = await Register.findOne({mobile_email:email_phone});
+    //console.log(user);
+    // const passwordMatch = await bcrypt.compare(pass, user.password);
+    // console.log(user.password)
+    if(user){
+    const passwordMatch = await bcrypt.compare(pass, user.password)
+        .then(result => {
+            //console.log(result)
+            return result;
+        })
+        .catch(err => {
+            console.log(err)
+        })
+        if (passwordMatch) {
+            res.render('user', {user})
+            //console.log(user);
+            //res.status(200).send(user);
+            // res.redirect(`/login/user/${user.firstname}${user.surname}`)
+        }
+        else{
+            res.status(401).send('Invalid password');
+        }
+    }
+    else{
+        console.log("Invalid username or password");
+        res.status(500).send('Invalid username or password');
+    }
+} catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+}
+});
+
+// app.get(`login/user/:username`, (req, res)=>{
+//     const username = req.user;
+//     // Register.findOne({mobile_email:username}, (err,user)=>{
+//     //      console.log(user)
+//     // })
+//     console.log(username)
+// });
+
+
 
 
 const port = 3000;
